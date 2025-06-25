ee0d9a1aad7e2e203fffa40a7be3e56c611ca1e3
Hiếu Nguyễn <hieunguyen.it613@gmail.com>
2025-06-25 03:00:51 +0000

    Không hiển thị dữ liệu chỉ có bộ khung

--- src/app/api/proxy/[...path]/route.ts
+++ src/app/api/proxy/[...path]/route.ts
@@ -2,7 +2,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 
 // Cập nhật URL API mới
-const API_URL = "https://rnxnj-118-71-152-58.a.free.pinggy.link"
+const API_URL = "https://rnqej-1-55-252-133.a.free.pinggy.link"
 
 async function proxyRequest(request: NextRequest, { params }: { params: { path: string[] } }, method: "GET" | "POST" | "PUT" | "DELETE" | "PATCH") {
   const path = params.path.join("/")
